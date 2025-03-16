20f91e47973137601433807aab165a829ec7e873
Kleber <kleber@user>
2025-03-16 15:19:38 -0300

    typográfia e ui

--- script.js
+++ script.js
@@ -51,57 +51,57 @@ function codigopix(x) {
     <p>Chave pix: kamilalimasilva354@gmail.com</p>`
     break
     case 5: 
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Mirante Dona Marta - R$100</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 6:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Pão de açucar - R$200</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 7:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Parque Lage - R$60</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 8:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Real Gabinete PT - R$60</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 9:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Caminho Niemeyer - R$60</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 10:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Campo São Bento Niterói - R$30</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 11:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Ilha da Boa Viagem Niterói - R$30</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 12:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Museu Arte Conteporânia - R$50</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 13:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Solar Jambeiro Niterói - R$30</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 14:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Teatro Municipal Niterói - R$40</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
     case 15:
-        document.getElementById('pix').innerText = 
+        document.getElementById('pix').innerHTML = 
         `<h1>Trilha das andorinhas Niterói - R$70</h1>
         <p>Chave Pix: kamilalimasilva354@gmail.com</p>`
         break
